e6d8f1beae905d47f93ef5e773c4b6cea82312f4
David Surumen <davidsurumen@gmail.com>
2023-07-14 05:12:28 +0300

    task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- script that creates table of unique users with country column
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255),
+        country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+);
