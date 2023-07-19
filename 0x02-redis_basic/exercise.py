072f20d8f8195ad3c26141d77098149524b1460b
David Surumen <davidsurumen@gmail.com>
2023-07-20 01:54:22 +0300

    task 0 soln

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Redis Operations Module
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """
+    Defines the methods to cache data in Redis
+    """
+
+    def __init__(self):
+        """ constructor """
+        self._redis = redis.Redis()
+        self._redis.flushdb
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Stores input data in redis using a random key
+
+        Args:
+            data: string | bytes | int | float
+
+        Return:
+            key: string that represents the key
+        """
+        key: str = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
